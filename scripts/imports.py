2a211b9eb6d0fd7ba38302fd1356f7a19dccf2c4
tefycatx-rgb <tefycatx@gmail.com>
2026-03-20 08:08:15 -0400

    Add files via upload

---
+++ scripts/imports.py
@@ -0,0 +1,8 @@
+
+# test 
+#from preprocess_utils import extract_features_from_image
+#image_path = "C:/Users/tefyc/Downloads/live_project_images/01300169/01300169_001.jpg"
+#print(extract_features_from_image(image_path))
+
+import os
+print("Current working directory:", os.getcwd())
